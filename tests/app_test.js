5a0b7cac16488dbbdeb6c1d7d0251b41fd299854
Youhana-Hana <meyouhana@gmail.com>
2013-11-20 22:51:34 +0000

    [#61165606] format app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -24,20 +24,20 @@ describe('logging', function() {
 		});
 
   it('should log configure', function(){
-			app.configure();
-			assert.equal('app.configure', logger.verbose.args[0]);
-		});
+     app.configure();
+     assert.equal('app.configure', logger.verbose.args[0]);
+   });
 
 });
 
 describe('settings', function() {
+  
   it('should set settings', function(){
-			app.configure();
-			
-			assert.equal('ejs', app.get('view engine'));
-			assert.equal('/home/Youhana/src/WorldRss/src/views', app.get('views'));
-			assert.equal('3000', app.get('port'));
-		});
+    app.configure();
+    assert.equal('ejs', app.get('view engine'));
+    assert.equal('/home/Youhana/src/WorldRss/src/views', app.get('views'));
+    assert.equal('3000', app.get('port'));
+  });
 
 });
 
