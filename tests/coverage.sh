3c5f3439f08eca340292f14be86a9cb140a182df
Youhana-Hana <meyouhana@gmail.com>
2013-11-24 14:49:46 +0000

    [#61343756] add client coverage check

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -3,7 +3,18 @@ rm -r ./coverage
 
 set -e
 
+echo 'Running server coverage...'
+
 ./node_modules/istanbul/lib/cli.js cover ./node_modules/mocha/bin/_mocha -- -R spec ./tests/server/*.js
 
 ./node_modules/istanbul/lib/cli.js check-coverage --statement 100
+
+echo 'Running client  coverage...'
+rm -r ./coverage
+
+./node_modules/karma/bin/karma start ./tests/client/karma.conf
+
+./node_modules/istanbul/lib/cli.js report text-summary --dir client --root ./coverage
+
+./node_modules/istanbul/lib/cli.js check-coverage --statement 100 --dir client --root ./coverage
 
