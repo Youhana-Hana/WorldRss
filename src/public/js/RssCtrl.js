03d9a6694f1f1220fa77d044c13f294937c7d3be
Youhana-Hana <meyouhana@gmail.com>
2013-11-24 15:27:09 +0000

    [#61343756] completed client tests

--- src/public/js/RssCtrl.js
+++ src/public/js/RssCtrl.js
@@ -5,12 +5,10 @@ function RssCtrl($scope, $http) {
     $http.get('http://localhost:3000/api/1/rss?src=' + sources)
       .success(function(data, status) {
         $scope.feeds = data;
-        $scope.status = status;
       })
       .error(function(data, status) {
         $scope.feeds = [];
         $scope.failure = data || "Request failed";
-        $scope.status = status;
       });
   };
 }
--- tests/client/RssCtrl_test.js
+++ tests/client/RssCtrl_test.js
@@ -1,30 +1,63 @@
 describe('RssCtrl', function() {
   var $httpBackend, $rootScope, createController;
-  
-  beforeEach(inject(function($injector) {
-    $httpBackend = $injector.get('$httpBackend');
-    $httpBackend.when('GET', 'http://localhost:3000/api/1/rss?src=url')
-                             .respond([{'rss': 'content'}]);
-    $rootScope = $injector.get('$rootScope');
-    var $controller = $injector.get('$controller');
+
+    beforeEach(inject(function($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+      $rootScope = $injector.get('$rootScope');
+      var $controller = $injector.get('$controller');
    
-    createController = function() {
-      return $controller('RssCtrl', {'$scope' : $rootScope });
-    };
+      createController = function() {
+       return $controller('RssCtrl', {'$scope' : $rootScope });
+      };
+ 
+    }));
+ 
+  describe('$http succeeded', function() { 
+    var response = {'rss': 'content'};
+   
+     beforeEach(inject(function($injector) {
+      $httpBackend.when('GET', 'http://localhost:3000/api/1/rss?src=url')
+                             .respond(response);
+    }));
 
-  }));
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
 
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
+    it('should fetch data', function() {
+      $httpBackend.expectGET('http://localhost:3000/api/1/rss?src=url');
+      controller = createController();
+    
+      $rootScope.fetch('url');
+      $httpBackend.flush();
+      expect($rootScope.feeds).toBe(response);
+    });
+
   });
 
-  it('should fetch data', function() {
-    $httpBackend.expectGET('http://localhost:3000/api/1/rss?src=url');
-    controller = createController();
+  describe('$http failed', function() { 
+   
+     beforeEach(inject(function($injector) {
+      $httpBackend.when('GET', 'http://localhost:3000/api/1/rss?src=url')
+                             .respond(500, 'error!');
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should fetch data', function() {
+      $httpBackend.expectGET('http://localhost:3000/api/1/rss?src=url');
+      controller = createController();
     
-    $rootScope.fetch('url');
-    $httpBackend.flush();
+      $rootScope.fetch('url');
+      $httpBackend.flush();
+      $rootScope.feeds.should.be.empty;
+      expect($rootScope.failure).toBe('error!');
+    });
+
   });
 
 });
