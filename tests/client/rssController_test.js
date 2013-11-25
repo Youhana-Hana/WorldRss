13842b36f680377349ece13e84ac9483353f25b8
Youhana-Hana <meyouhana@gmail.com>
2013-11-25 21:02:36 +0000

    [#61426294] fixes client coverage check

--- tests/client/rssController_test.js
+++ tests/client/rssController_test.js
@@ -22,3 +22,28 @@ describe('rssController', function() {
   });
 
 });
+
+describe('rssService', function () {
+  var rssService, $httpBackend;
+  beforeEach(angular.mock.module('rssModule'));
+
+  beforeEach(function () {
+    angular.mock.inject(function ($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+      rssService = $injector.get('rssService');
+    });
+  });
+
+  describe('query', function () {
+    it('should call rss with url', inject(function (rssService) {
+      $httpBackend.expectGET('/api/1/rss?src=URL')
+              .respond([ { rss : "content" } ]);
+
+      var result = rssService.query({sources: "URL"});
+      $httpBackend.flush();
+
+      expect(result[0].rss).toEqual('content');
+    }));
+
+  });
+});
